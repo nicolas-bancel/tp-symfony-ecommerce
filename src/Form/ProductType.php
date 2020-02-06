08bc7a42519c86cdbcc15f676a259810cab11986
Nicolas Bancel <bancelnicolas@gmail.com>
2020-02-06 14:09:37 +0100

    index and product crud

---
+++ src/Form/ProductType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Product;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProductType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('slug')
+            ->add('description')
+            ->add('price')
+            ->add('date')
+            ->add('heart')
+            ->add('color')
+            ->add('image')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Product::class,
+        ]);
+    }
+}
